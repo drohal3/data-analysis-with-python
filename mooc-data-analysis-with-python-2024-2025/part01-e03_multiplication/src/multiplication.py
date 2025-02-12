e61ef1a4359510290b211c154aa0f5564e2213f7
Dominik Rohal <rohal.dominik.123@gmail.com>
2025-02-12 12:41:38 +0200

    part01-e03

---
+++ mooc-data-analysis-with-python-2024-2025/part01-e03_multiplication/src/multiplication.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def main():
+    # Enter your solution here
+    multiplicand = 4
+    for multiplier in range(11):
+        print(f"{multiplicand} multiplied by {multiplier} is {multiplicand * multiplier}")
+
+if __name__ == "__main__":
+    main()
