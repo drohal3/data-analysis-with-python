46c706aea091fbb4fc3f998840707c4dd2fff8a7
Dominik Rohal <rohal.dominik.123@gmail.com>
2025-02-19 12:25:44 +0200

    until part02e01

---
+++ mooc-data-analysis-with-python-2024-2025/part03-e01_column_comparison/src/column_comparison.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+def column_comparison(a):
+    ca = a[:,1]
+    cb = a[:,-2]
+    mask = ca > cb
+
+    return a[mask]
+    
+def main():
+    a = np.array([
+        [8, 9, 3, 8, 8],
+        [0, 5, 3, 9, 9],
+        [5, 7, 6, 0, 4],
+        [7, 8, 1, 6, 2],
+        [2, 1, 3, 5, 8]
+    ])
+    print(column_comparison(a))
+
+
+if __name__ == "__main__":
+    main()
