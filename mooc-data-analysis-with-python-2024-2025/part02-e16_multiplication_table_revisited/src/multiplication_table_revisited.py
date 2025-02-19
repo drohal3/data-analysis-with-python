4d8729b34fb95f6173d0c4fe592cc582230ff981
Dominik Rohal <rohal.dominik.123@gmail.com>
2025-02-19 11:54:09 +0200

    part2 completed

---
+++ mooc-data-analysis-with-python-2024-2025/part02-e16_multiplication_table_revisited/src/multiplication_table_revisited.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+def multiplication_table(n):
+    arr = np.arange(n)
+    return arr * arr.reshape(-1, 1)
+
+def main():
+    print(multiplication_table(4))
+
+if __name__ == "__main__":
+    main()
