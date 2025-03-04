762bb590de5196bef50cf56d82810de26feb9939
Dominik Rohal <rohal.dominik.123@gmail.com>
2025-03-04 12:23:30 +0200

    until part04e08 .

--- mooc-data-analysis-with-python-2024-2025/part04-e08_subsetting_by_positions/src/subsetting_by_positions.py
+++ mooc-data-analysis-with-python-2024-2025/part04-e08_subsetting_by_positions/src/subsetting_by_positions.py
@@ -5,7 +5,7 @@ import pandas as pd
 def subsetting_by_positions():
     df = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep="\t")
     columns = [2, 3]
-    return df.iloc[0:10, columns]
+    return df.iloc[:10, columns]
 
 def main():
     print(subsetting_by_positions())
