cebf6a42210fe682397026b8e790fa40d97250fd
Dominik Rohal <rohal.dominik.123@gmail.com>
2025-03-04 12:22:29 +0200

    until part04e08

---
+++ mooc-data-analysis-with-python-2024-2025/part04-e08_subsetting_by_positions/src/subsetting_by_positions.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+def subsetting_by_positions():
+    df = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep="\t")
+    columns = [2, 3]
+    return df.iloc[0:10, columns]
+
+def main():
+    print(subsetting_by_positions())
+
+
+if __name__ == "__main__":
+    main()
