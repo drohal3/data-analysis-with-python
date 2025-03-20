d73d8ab2858161e1ba76506848ee7e9f49543f18
Dominik Rohal <rohal.dominik.123@gmail.com>
2025-03-20 17:18:02 +0200

    until part05e02

---
+++ mooc-data-analysis-with-python-2024-2025/part05-e02_cycling_weather/src/cycling_weather.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+def split_date_continues():
+    days = dict(zip("ma ti ke to pe la su".split(), "Mon Tue Wed Thu Fri Sat Sun".split()))
+    months = dict(zip("tammi helmi maalis huhti touko kesä heinä elo syys loka marras joulu".split(), range(1, 13)))
+    df = pd.read_csv("src/Helsingin_pyorailijamaarat.csv", sep=";")
+    df = df.dropna(axis=0, how="all").dropna(axis=1, how="all")
+    d = df["Päivämäärä"].str.split(expand=True)
+    d.columns = ["Weekday", "Day", "Month", "Year", "Hour"]
+
+    hourmin = d["Hour"].str.split(":", expand=True)
+    d["Hour"] = hourmin.iloc[:, 0]
+
+    d["Weekday"] = d["Weekday"].map(days)
+    d["Month"] = d["Month"].map(months)
+
+    d = d.astype({"Weekday": object, "Day": int, "Month": int, "Year": int, "Hour": int})
+
+    return pd.concat([d, df], axis=1).drop('Päivämäärä', axis=1)
+
+def cycling_weather():
+    cycling_df = split_date_continues()
+    weather_df = pd.read_csv("src/kumpula-weather-2017.csv", sep=",")
+
+    return (pd.merge(cycling_df, weather_df, left_on=["Year", "Month", "Day"], right_on=["Year", "m", "d"])
+            .drop(["m", "d", "Time", "Time zone"], axis=1))
+
+def main():
+    print(cycling_weather())
+
+
+if __name__ == "__main__":
+    main()
