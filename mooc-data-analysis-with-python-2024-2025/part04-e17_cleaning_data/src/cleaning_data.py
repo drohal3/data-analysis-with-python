030724b4865e57250241786df71b7c0d6dedb650
Dominik Rohal <rohal.dominik.123@gmail.com>
2025-03-20 13:25:33 +0200

    until part04e17, part04 completed

---
+++ mooc-data-analysis-with-python-2024-2025/part04-e17_cleaning_data/src/cleaning_data.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import numpy as np
+
+PATH = "src/presidents.tsv"
+
+STR_NUM = {
+
+}
+
+def cleaning_data():
+    def clean_names(names):
+        names = names.str.split(",")
+        names = names.apply(lambda x: ' '.join(x[::-1]).strip())
+        names = names.str.title()
+        return names
+
+    df = pd.read_csv(PATH, sep="\t")
+    df["President"] = clean_names(df["President"])
+    df["Vice-president"] = clean_names(df["Vice-president"])
+    df["Start"] = df["Start"].str[:4]
+    df.replace({"Last": {"-": np.nan}, "Seasons": {"two": "2"}}, inplace=True)
+    df = df.astype({"President": object, "Start": int, "Last": float, "Seasons": int, "Vice-president": object})
+    return df
+
+
+def main():
+    print(cleaning_data())
+
+if __name__ == "__main__":
+    main()
