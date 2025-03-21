66bc1160146728798784faff7649ee6a119e5611
Dominik Rohal <rohal.dominik.123@gmail.com>
2025-03-21 14:39:13 +0200

    until part05e04

---
+++ mooc-data-analysis-with-python-2024-2025/part05-e04_cyclists_per_day/src/cyclists_per_day.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def split_date(col):
+    days = dict(zip("ma ti ke to pe la su".split(), "Mon Tue Wed Thu Fri Sat Sun".split()))
+    months = dict(zip("tammi helmi maalis huhti touko kesä heinä elo syys loka marras joulu".split(), range(1, 13)))
+    d = col.str.split(expand=True)
+    d.columns = ["Weekday", "Day", "Month", "Year", "Hour"]
+
+    hourmin = d["Hour"].str.split(":", expand=True)
+    d["Hour"] = hourmin.iloc[:, 0]
+
+    d["Weekday"] = d["Weekday"].map(days)
+    d["Month"] = d["Month"].map(months)
+
+    return d.astype({"Weekday": object, "Day": int, "Month": int, "Year": int, "Hour": int})
+
+def cyclists_per_day():
+    df = pd.read_csv("src/Helsingin_pyorailijamaarat.csv", sep=";")
+    df.dropna(how="all", inplace=True)
+    df.dropna(axis=1, how="all", inplace=True)
+    d = split_date(df["Päivämäärä"])
+
+    df = pd.concat([d[["Year", "Month", "Day"]], df], axis=1).drop(columns=["Päivämäärä"])
+    return df.groupby(["Year", "Month", "Day"], as_index=True).sum()
+    
+def main():
+    data = cyclists_per_day()
+    august = data.loc[(2017, 8), :]
+    august.plot()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
